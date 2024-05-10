b52cf86f7a5b0cffdfbeeb0532aa2197484ae8fc
Adam Ługowski <adam.lugowski@live.com>
2024-05-10 14:39:54 +0200

    added feature to use digits in password

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from random import choice
-import string
+from string import ascii_letters, digits
 
 
 def password_generator(number_of_characters):
-    password_characters = []
+    """
+    This function is creating the password using randomly chosen big letters, small letters and digits
+    :param number_of_characters: Number of characters that password should have
+    :return: Random password
+    """
+    list_of_characters = []
+    password_characters = ascii_letters + digits
     for _ in range(number_of_characters):
-        password_characters.append(choice(string.ascii_letters))
+        list_of_characters.append(choice(password_characters))
 
-    password = ''.join(password_characters)
+    password = ''.join(list_of_characters)
+
     return password
 
 
-print(password_generator(5))
+print(password_generator(10))
