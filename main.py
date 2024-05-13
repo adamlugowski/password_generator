205545e45e9005519337c603ae6ea367eaad4850
Adam Ługowski <adam.lugowski@live.com>
2024-05-13 20:49:48 +0200

    used getpass to hide user input due to security

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from getpass import getpass
 from random import choice
 from string import ascii_letters, digits
 
@@ -20,11 +21,18 @@ def password_generator(number_of_characters):
 
 def main():
     try:
-        user_input = int(input('Enter number of characters: '))
+        user_input = int(getpass('Enter number of characters: '))
+        if user_input <= 0:
+            raise ValueError("Number of characters should be a positive integer.")
         print("It's important that this password is shown only once")
-        print(f'This is your password: {password_generator(user_input)}')
-    except ValueError:
-        print('You should type integer.')
+        password = password_generator(user_input)
+        if user_input > 100:
+            user_input = 100
+            print('Maximum password length is 100 characters. Generating password with 100 characters.')
+        print(f'This is your password: {password}')
+        return password
+    except ValueError as e:
+        print(str(e))
     finally:
         print('Take care of your password.')
 
